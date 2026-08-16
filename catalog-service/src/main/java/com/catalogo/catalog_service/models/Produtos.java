be1b7ba5b2440c9ac3d8b951d13483701e9c71ca
LucasHAlbergaria <lucashenriquealbergaria01@gmail.com>
2026-08-16 11:54:05 -0300

    feat: Adicionado o package models e implementação da classe Produtos

---
+++ catalog-service/src/main/java/com/catalogo/catalog_service/models/Produtos.java
@@ -0,0 +1,87 @@
+package com.catalogo.catalog_service.models;
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_product")
+
+public class Produtos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private int stock;
+
+    public Produtos(Long id, String name, Double price, String category, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.stock = stock;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Produtos produtos = (Produtos) o;
+        return Objects.equals(id, produtos.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
+
+
